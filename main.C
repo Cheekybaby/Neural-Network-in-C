7cce36492d23a0149d67f21398dcf5dc9efad700
Shashank Ranjan <shashankranjan07@gmail.com>
2026-02-25 10:18:31 +0530

    Add: Basic skeleton and MNIST bin files

---
+++ main.C
@@ -0,0 +1,12 @@
+/*
+Headers:
+    stdio.h: for input/output functions like printf and scanf.
+    stdlib.h: for general utility functions like malloc, free, and exit.
+    math.h: for mathematical functions like sqrt, pow, and trigonometric functions.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
